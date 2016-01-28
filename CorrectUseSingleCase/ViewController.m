3e8606836f065250156b479baedd42dc0545fa5f
yajie <yajie.yao@imike.com>
2016-01-28 22:52:11 +0800

    singleInstance

--- CorrectUseSingleCase/ViewController.m
+++ CorrectUseSingleCase/ViewController.m
@@ -20,20 +20,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    //"单例就是披着羊皮的全局状态"。一个单例可以被使用在任何地方，而不需要显式地声明依赖。
      //单例应该只用来保存全局的状态，并且不能和任何作用域绑定。如果这些状态的作用域比一个完整的应用程序的生命周期要短，那么这个状态就不应该使用单例来管理。用一个单例来管理用户绑定的状态，不是很好的设计方式，需要重新评估自己的设计问题。
 
     MikeMember  *member = [[MikeMember alloc]init];
-    [self.profileImage sd_setImageWithPreviousCachedImageWithURL:[NSURL URLWithString:@"https://avatars1.githubusercontent.com/u/10678821?v=3&s=460"] placeholderImage:[UIImage imageNamed:@""] options:SDWebImageProgressiveDownload progress:^(NSInteger receivedSize, NSInteger expectedSize) {
+    [self.profileImage sd_setImageWithPreviousCachedImageWithURL:[NSURL URLWithString:@"https://github.com/summerHearts/TaskManager/blob/master/TaskManager/Image/4.jpg?raw=true"] placeholderImage:[UIImage imageNamed:@""] options:SDWebImageProgressiveDownload progress:^(NSInteger receivedSize, NSInteger expectedSize) {
      
     } completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
         [member setProfileImageByUserID:@"12" imageData:UIImageJPEGRepresentation(image,0.9)];
     }];
-    
 
-    
-    
-    
 }
 
 - (void)didReceiveMemoryWarning {
